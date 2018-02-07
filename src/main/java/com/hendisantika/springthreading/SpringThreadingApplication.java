4750dfc4d64fc75f86c4cdba4013f4309d86894f
Hendi Santika <hendisantika@gmail.com>
2018-02-08 06:53:03 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springthreading/SpringThreadingApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springthreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThreadingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hendisantika/springthreading/SpringThreadingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springthreading;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringThreadingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
