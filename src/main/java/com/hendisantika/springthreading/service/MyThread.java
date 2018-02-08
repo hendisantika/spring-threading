86c3b72e170fdacf0ca6c39e16650d55c1ba1af4
Hendi Santika <hendisantika@gmail.com>
2018-02-08 07:02:53 +0700

    #6 - Add My Thread Class

---
+++ src/main/java/com/hendisantika/springthreading/service/MyThread.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springthreading.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-threading
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/18
+ * Time: 07.02
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Scope("prototype")
+public class MyThread implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyThread.class);
+
+    @Override
+    public void run() {
+
+        LOGGER.info("Called from thread");
+    }
+}
