a57ba19d2b90ddd37390912972508da168caabcf
Hendi Santika <hendisantika@gmail.com>
2018-02-08 07:03:14 +0700

    #7 - Add Asynchronous Service

---
+++ src/main/java/com/hendisantika/springthreading/service/AsynchronousService.java
@@ -0,0 +1,60 @@
+package com.hendisantika.springthreading.service;
+
+import com.hendisantika.springthreading.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-threading
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/18
+ * Time: 07.01
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class AsynchronousService {
+
+    @Autowired
+    private TaskExecutor taskExecutor;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Async
+    @Transactional
+    public void printEmployees() {
+
+        List<Employee> employees = entityManager.createQuery("SELECT e FROM Employee e").getResultList();
+        employees.stream().forEach(e -> System.out.println(e.getEmail()));
+    }
+
+    @Async("specificTaskExecutor")
+    @Transactional
+    public CompletableFuture<List<Employee>> fetchEmployess() {
+        List<Employee> employees = entityManager.createQuery("SELECT e FROM Employee e").getResultList();
+        return CompletableFuture.completedFuture(employees);
+    }
+
+
+    public void executeAsynchronously() {
+
+        MyThread myThread = applicationContext.getBean(MyThread.class);
+        taskExecutor.execute(myThread);
+    }
+
+}
