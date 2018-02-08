8c4f3b543a0c6097526ece4b88d98850f3b2d2cc
Hendi Santika <hendisantika@gmail.com>
2018-02-08 07:00:23 +0700

    #3 - Add Thread Config

---
+++ src/main/java/com/hendisantika/springthreading/config/ThreadConfig.java
@@ -0,0 +1,45 @@
+package com.hendisantika.springthreading.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-threading
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/02/18
+ * Time: 07.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableAsync
+public class ThreadConfig {
+
+    @Bean
+    @Primary
+    public TaskExecutor threadPoolTaskExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(4);
+        executor.setThreadNamePrefix("default_task_executor_thread");
+        executor.initialize();
+
+        return executor;
+    }
+
+    @Bean(name = "specificTaskExecutor")
+    public TaskExecutor specificTaskExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.initialize();
+        return executor;
+    }
+
+}
